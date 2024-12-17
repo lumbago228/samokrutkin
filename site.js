0df630ad4de28d3a9b263ef5aa1c366c5a0bbd1e
lumbago228 <iluasergeevi@Gmail.com>
2024-12-17 21:00:04 +0300

    Update site.js

--- site.js
+++ site.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const parallaxContainer = document.querySelector('.parallax-container');
 
-    window.addEventListener('scroll', function() {
-        const scrollPosition = window.scrollY;
-        parallaxContainer.style.transform = `translateY(${scrollPosition * 0.3}px)`;
-    });
-});
